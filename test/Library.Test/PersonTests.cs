28f624299180069eaaa999460e0ed724b1f598e5
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-04-19 20:52:31 -0300

    Add tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -6,16 +6,48 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        [TestCase("A", "43644237")]
+        [TestCase("Pepe López", "4.364.423-7")]
+        public void DadosCédulaYNombreVálidos_AsignaLosValores(string nombre, string ci)
         {
-            // Insertá tu código de inicialización aquí
+            var p = new Person(nombre, ci);
+
+            Assert.AreEqual(p.Name, nombre);
+            Assert.AreEqual(p.ID, ci);
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        [TestCase("", "43644237")]
+        [TestCase(null, "4.364.423-7")]
+        public void DadoNombreInválidoYCédulaVálida_NoAsignaElNombreYAsignaCédula(string nombre, string ci)
         {
-            // Insertá tu código  de pruebaaquí
+            var p = new Person(nombre, ci);
+
+            Assert.AreEqual(p.Name, null);
+            Assert.AreEqual(p.ID, ci);
+        }
+
+        [Test]
+        [TestCase("A", "12345678")]
+        [TestCase("B C", "1.234.567-8")]
+        public void DadoNombreVálidoYCédulaInválida_AsignaNombreYNoAsignaCédula(string nombre, string ci)
+        {
+            var p = new Person(nombre, ci);
+
+            Assert.AreEqual(p.Name, nombre);
+            Assert.AreEqual(p.ID, null);
+        }
+
+        [Test]
+        [TestCase("", "12345678")]
+        [TestCase(null, "1.234.567-8")]
+        public void DadoNombreInválidoYCédulaInválida_NoAsignaNombreYNoAsignaCédula(string nombre, string ci)
+        {
+            var p = new Person(nombre, ci);
+
+            Assert.AreEqual(p.Name, null);
+            Assert.AreEqual(p.ID, null);
         }
     }
 }
